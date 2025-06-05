8a430f7eb571ee0d4607ad6856072d0987a24fe7
Masaomi Hatakeyama <masaomi.hatakeyama@uzh.ch>
2025-06-05 15:16:28 +0200

    Added backend/Gemfile, minimal set

---
+++ git-init.sh
@@ -0,0 +1,7 @@
+echo "# new_SUSHI_2025" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:masaomi/new_SUSHI_2025.git
+git push -u origin main
