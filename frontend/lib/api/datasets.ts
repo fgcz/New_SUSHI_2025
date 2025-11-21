a5b7f8d40323cdb66fb74cbe69115a5c1ee5cc3f
Ronald Domi <ronald.domi@uzh.ch>
2025-11-21 11:24:17 +0100

    1) Working version of HandsOnTable for sample editing 2) Adding of confirmation page for a job submission 3) Removing redundant API calls and API types 4) Maintaining documentation

--- frontend/lib/api/datasets.ts
+++ frontend/lib/api/datasets.ts
@@ -1,13 +1,13 @@
 import { httpClient } from './client';
-import { DatasetsResponse, Dataset, CreateDatasetResponse, DatasetSamplesResponse, DatasetRunnableAppsResponse, DatasetTreeResponse} from '../types/dataset';
+import { DatasetsResponse, ProjectDataset, DatasetTreeResponse} from '../types/dataset';
 
 export const datasetApi = {
     async getDatasets(): Promise<DatasetsResponse> {
         return httpClient.request<DatasetsResponse>('/api/v1/datasets');
     },
 
-    async getDataset(id: number): Promise<Dataset> {
-        return httpClient.request<Dataset>(`/api/v1/datasets/${id}`);
+    async getDataset(id: number): Promise<ProjectDataset> {
+        return httpClient.request<ProjectDataset>(`/api/v1/datasets/${id}`);
     },
 
     // async createDataset(name: string): Promise<CreateDatasetResponse> {
@@ -19,14 +19,5 @@ export const datasetApi = {
 
     async getDatasetTree(id: number): Promise<DatasetTreeResponse> {
         return httpClient.request<DatasetTreeResponse>(`/api/v1/datasets/${id}/tree`);
-    },
-
-
-    async getDatasetSamples(id: number): Promise<DatasetSamplesResponse> {
-        return httpClient.request<DatasetSamplesResponse>(`/api/v1/datasets/${id}/samples`);
-    },
-
-    async getRunnableApps(id: number): Promise<DatasetRunnableAppsResponse> {
-        return httpClient.request<DatasetRunnableAppsResponse>(`/api/v1/datasets/${id}/runnable_apps`);
     },
 };
